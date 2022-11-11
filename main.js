859f4f3a51ba0adf295da934505243303b0c8c75
AndreaPanzeriDev <andpanz@libero.it>
2022-11-11 17:32:43 +0100

    auto-save

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var app = new Vue({
-    el: "#root",
+    el: "#app",
     data: {
         image: [
             {landscape: 'https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'},
@@ -8,5 +8,17 @@ var app = new Vue({
             {landscape: 'https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'}
         ],
         position : 0,
-    }
+    },
+    methods: {
+        goOn: function(){
+            if(this.position == 2){
+                this.position = 0;
+            }else{
+                this.position++;
+            }   
+        },
+        goBack: function(){
+            
+        }
+    },
 });
