cc0ff07e394fa903e52e8e7870f3531b5e9aa3b9
AndreaPanzeriDev <andpanz@libero.it>
2022-11-11 16:24:33 +0100

    inizializzazione

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var app = new Vue({
+    el: "#root",
+    data: {
+        image: [
+            {img1: 'https://images.unsplash.com/photo-1587502537147-2ba64a62e3d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2796&q=80'},
+            {img2: 'https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'},
+            {img3: 'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'},
+        ]
+    }
+});
